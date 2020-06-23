e1d179124cdabc519ec92c2636da272a82a110b6
Jason Hibbs <jason@jasonhibbs.co.uk>
2020-06-23 18:52:45 +0100

    add units to list

--- src/components/ListItemTime.vue
+++ src/components/ListItemTime.vue
@@ -18,6 +18,7 @@ export default class ListItemTime extends Vue {
     id: string
     label: string
     datetime: string
+    display?: string
   }
 
   get id() {
@@ -28,10 +29,25 @@ export default class ListItemTime extends Vue {
     return this.time.label
   }
 
+  get displayUnits() {
+    return this.time.display && this.time.display !== 'auto'
+      ? this.time.display
+      : undefined
+  }
+
   get timeString() {
     const then = new Date(this.time.datetime)
     const now = new Date()
-    return formatDistanceStrict(then, now, { addSuffix: true })
+    const options: any = {
+      addSuffix: true,
+      unit: this.displayUnits,
+    }
+    const string = formatDistanceStrict(then, now, options)
+    return this.parseThousands(string)
+  }
+
+  parseThousands(string: string) {
+    return string.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
   }
 }
 </script>
